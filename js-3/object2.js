5a26b208a01b7a2d699928350d8508710f9b24fe
Katherine <angiecruzo.o@gmail.com>
2023-03-16 22:06:34 -0500

    solucion a los ejercicios de js

---
+++ js-3/object2.js
@@ -0,0 +1,17 @@
+let persona = {
+  nombre: "",
+  edad: null,
+  ciudad: "",
+  profesion: "",
+};
+console.log(persona);
+function presentacion(objeto) {
+  return `Nombre: ${objeto.nombre}, Edad: ${objeto.edad}; Ciudad: ${objeto.ciudad}; profesion: ${objeto.profesion}`;
+}
+let mensaje = presentacion(persona);
+console.log(mensaje);
+persona.hobbies = ["leer", "cocinar", "nadar", "estudiar"];
+console.log(persona.hobbies);
+for (let i = 0; i <= persona.hobbies.length - 1; i++) {
+  console.log(persona.hobbies[i]);
+}
